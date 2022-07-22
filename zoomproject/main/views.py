c34e53f391e40550d5ac888122a21f0a764f7c20
shikhar thapliyal <shikhar1707@gmail.com>
2022-07-22 16:29:18 +0530

    hash added

--- zoomproject/main/views.py
+++ zoomproject/main/views.py
@@ -2,10 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import logging
 from main.tasks import handle_queue
+import hmac
+import hashlib
+import base64
 
 logger = logging.getLogger(__name__)
 
 CONTENT_JSON = "application/json"
+TOKEN = "JpQZyHhkQAGryrNboPFGhQ"
 
 
 class CallbackView(APIView):
@@ -15,10 +19,27 @@ class CallbackView(APIView):
 
     def post(self, request):
         try:
-            handle_queue.apply_async(
-                args=[request.data], queue="request_queue"
-            )
-            return Response(data={"message": "success"}, status=200, content_type=CONTENT_JSON)
+            if request.data["event"] == "endpoint.url_validation":
+                plain_token = request.data["payload"]["plainToken"]
+                digest = hmac.new(
+                    TOKEN.encode("utf-8"),
+                    msg=plain_token.encode("utf-8"),
+                    digestmod=hashlib.sha256).digest()
+                signature = base64.b64encode(digest).decode()
+
+                return Response(
+                    data={
+                        "encryptedToken": signature,
+                        "plainToken": plain_token
+                    },
+                    status=200,
+                    content_type=CONTENT_JSON
+                )
+            else:
+                handle_queue.apply_async(
+                    args=[request.data], queue="request_queue"
+                )
+                return Response(data={"message": "success"}, status=200, content_type=CONTENT_JSON)
         except Exception as exc:
             logger.exception("Exception: {}".format(str(exc)))
             return Response(data={"message": "success"}, status=400, content_type=CONTENT_JSON)
